fd4d3dc9e46917d44c2da46aea081c2284cbc761
Grace Shih <vocalise@gmail.com>
2018-09-04 17:30:21 +0000

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,4 +1,49 @@
 class FiguresController < ApplicationController
-  get '/new' do
+  
+  get '/figures' do
+    @figures = Figure.all
+    erb :'/figures/index'
+  end
+  
+  get '/figures/new' do
+    erb :'/figures/new'
+  end
+  
+  post '/figures' do
+    figure = Figure.create(params[:figure])
+    if !params[:title][:name].empty?
+      title = Title.create(name: params[:title][:name])
+      figure.titles << title
+    end
+    if !params[:landmark][:name].empty?
+      landmark = Landmark.create(name: params[:landmark][:name])
+      figure.landmarks << landmark
+    end
+    figure.save
+    redirect to '/figures'
+  end
+  
+  get '/figures/:id' do
+    @figure = Figure.find(params[:id])
+    erb :'/figures/show'
+  end
+  
+  get '/figures/:id/edit' do
+    @figure = Figure.find(params[:id])
+    erb :'/figures/edit'
+  end
+  
+  patch '/figures/:id' do
+    @figure = Figure.find(params[:id])
+    if !params[:title][:name].empty?
+      title = Title.create(params[:figure][:name])
+      @figure.titles << title
+    end
+    if !params[:landmark][:name].empty?
+      landmark = Landmark.create(params[:landmark][:name])
+      @figure.landmarks << landmark
+    end
+    @figure.update
+    redirect to "/figures/#{@figure.id}"
   end
 end
