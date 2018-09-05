dd196680ffbf2b1d8d5e16d57bfb4b50c4cff681
Grace Shih <vocalise@gmail.com>
2018-09-05 04:58:08 +0000

    automatically backed up by learn

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,4 +1,13 @@
 class LandmarksController < ApplicationController
   get '/landmarks' do
+    erb :'/landmarks/index'
+  end
+  
+  post '/landmarks' do
+    erb :'/landmarks/'
+  end
+  
+  get '/landmarks/:id' do
+    erb :'/landmarks/show
   end
 end
