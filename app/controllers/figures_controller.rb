ad23fc9a08da106c333ed4ffeb0650251bff5b79
Grace Shih <vocalise@gmail.com>
2018-09-05 03:33:40 +0000

    automatically backed up by learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,16 +1,20 @@
 class FiguresController < ApplicationController
+<<<<<<< HEAD
   
   get '/figures' do
     @figures = Figure.all
     erb :'/figures/index'
   end
   
+=======
+>>>>>>> 429264406d61d3461d7d849b842e2c5b63859146
   get '/figures/new' do
     erb :'/figures/new'
   end
   
   post '/figures' do
     figure = Figure.create(params[:figure])
+<<<<<<< HEAD
     if !params[:title][:name].empty?
       title = Title.create(name: params[:title][:name])
       figure.titles << title
@@ -45,5 +49,10 @@ class FiguresController < ApplicationController
     end
     @figure.update
     redirect to "/figures/#{@figure.id}"
+=======
+    binding.pry
+    figure.title_ids << figure.title
+    erb :'/figures'
+>>>>>>> 429264406d61d3461d7d849b842e2c5b63859146
   end
 end
